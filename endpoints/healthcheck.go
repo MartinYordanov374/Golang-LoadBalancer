8f7ab3b5dbcff418676c9413341fac98cbdbb7b6
MartinYordanov374 <martinyordanov284@gmail.com>
2026-07-14 13:00:07 -0400

    The healthcheck endpoint now returns json with status code and a message

--- endpoints/healthcheck.go
+++ endpoints/healthcheck.go
@@ -1,10 +1,16 @@
 package endpoints
 
 import (
-	"log"
 	"net/http"
+	"encoding/json"
 )
 
+type HealthCheckResponse struct {
+	StatusCode int	`json:"StatusCode"`
+	Message	   string	`json:"Message"`
+}
+
 func HealthCheck(w http.ResponseWriter, r *http.Request){
-	log.Printf("server is up")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(HealthCheckResponse{StatusCode: 200, Message: "The server is up!"})
 }
