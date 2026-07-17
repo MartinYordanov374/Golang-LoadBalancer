3aff0da92a4d80d183c4aae0eb8ff1c3d9ec8189
MartinYordanov374 <martinyordanov284@gmail.com>
2026-07-17 23:37:30 +0300

    Perform goroutines for each server health check.

--- loadbalancer/loadbalancer.go
+++ loadbalancer/loadbalancer.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/google/uuid"
 	"io"
 	"log"
-	"github.com/google/uuid"
+	"net/http"
+	"sync"
+	"time"
 )
 
 type Server struct {
-	ID string
+	ID   string
 	Host string
 	Port int
 	IsUp bool
 }
 
-func main(){
-	go func(){
+func main() {
+	go func() {
 		http.ListenAndServe(":8000", nil)
 	}()
 
 	ServersList := InitializeServersList()
 
-	PerformHealthCheck(ServersList["ServerOne"])
-
-
+	ticker := time.NewTicker(5 * time.Second)
+	for range ticker.C {
+		wg := new(sync.WaitGroup)
+		wg.Add(len(ServersList))
+		for _, Server := range ServersList {
+			go PerformHealthCheck(Server, wg)
+		}
+		wg.Wait()
+	}
 }
 
 func InitializeServersList() map[string]*Server {
@@ -31,43 +40,44 @@ func InitializeServersList() map[string]*Server {
 	Servers := make(map[string]*Server)
 
 	Servers["ServerOne"] = &Server{
-		ID: uuid.New().String(),
+		ID:   uuid.New().String(),
 		Host: "localhost",
 		Port: 8080,
 		IsUp: false}
 
 	Servers["ServerTwo"] = &Server{
-		ID: uuid.New().String(),
+		ID:   uuid.New().String(),
 		Host: "localhost",
-		Port: 8080,
+		Port: 8081,
 		IsUp: false}
 
 	Servers["ServerThree"] = &Server{
-		ID: uuid.New().String(),
+		ID:   uuid.New().String(),
 		Host: "localhost",
-		Port: 8080,
+		Port: 8082,
 		IsUp: false}
 
 	return Servers
 
 }
 
-func PerformHealthCheck(TargetServer *Server){
-	resp, err := http.Get("http://localhost:8080/health")
+func PerformHealthCheck(TargetServer *Server, WaitGroup *sync.WaitGroup) {
+
+	defer WaitGroup.Done()
+	uri := fmt.Sprintf("http://%s:%d/health", TargetServer.Host, TargetServer.Port)
+	resp, err := http.Get(uri)
 
 	if err != nil {
-		log.Println("Health check failed for server 1")
+		log.Println("Health check failed for ", TargetServer.ID)
 		log.Println(err)
-		select{}
+		return
 	}
 	defer resp.Body.Close()
 
 	jsonbody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		select{}
+		return
 	}
 	log.Println(string(jsonbody))
-
-	select{}
 }
