fb776246d0c371c3a3282098bf114278e0749ae1
MartinYordanov374 <martinyordanov284@gmail.com>
2026-07-30 12:56:41 -0400

    Remove mutex from the HandleServerDownCounter function because it is not needed

--- HelperFunctions/HandleServerDownCounter.go
+++ HelperFunctions/HandleServerDownCounter.go
@@ -9,14 +9,11 @@ func HandleServerDownCounter(TargetServer *Structs.Server){
 			log.Println("Skipping server ", TargetServer.Port)
 			// TODO: Figure out how to count the time until the server
 			// will be back to the scanning list
-			TargetServer.Mutex.Lock()
 			TargetServer.IsInCooldown.Store(true)
-			TargetServer.Mutex.Unlock()
 			TargetServer.DownCount.Store(0)
 			SetServerScanningCooldown(TargetServer)
 		}else{
-			TargetServer.Mutex.Lock()
 			TargetServer.DownCount.Add(1)
-			TargetServer.Mutex.Unlock()
+
 		}
 }
