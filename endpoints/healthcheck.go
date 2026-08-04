e6898f97fca87e6e6c1af4f3079b581fe465a54e
MartinYordanov374 <martinyordanov284@gmail.com>
2026-08-04 09:16:04 -0400

    The HealthCheck endpoint now uses the struct endpoint response struct

--- endpoints/healthcheck.go
+++ endpoints/healthcheck.go
@@ -3,14 +3,11 @@ package endpoints
 import (
 	"net/http"
 	"encoding/json"
+	"golang-loadbalancer/Structs"
 )
 
-type HealthCheckResponse struct {
-	StatusCode int	`json:"StatusCode"`
-	Message	   string	`json:"Message"`
-}
 
 func HealthCheck(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(HealthCheckResponse{StatusCode: 200, Message: "The server is up!"})
+	json.NewEncoder(w).Encode(Structs.HealthCheckEndpointResponse{StatusCode: 200, Message: "The server is up!"})
 }
